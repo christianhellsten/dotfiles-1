6c72ac42f659e27a39c9a3992196bb2747f50d08
christianhellsten <christian@aktagon.com>
2014-08-18 15:16:21 +0300

    Fixes

--- ruby/rbenv.zsh
+++ ruby/rbenv.zsh
@@ -1,6 +1,2 @@
-export PATH="$HOME/.rbenv/bin:$PATH"
-# init according to man page
-if (( $+commands[rbenv] ))
-then
-  eval "$(rbenv init -)"
-fi
+export PATH="$HOME/.rbenv/bin:~/.rbenv/shims:$PATH"
+eval "$(rbenv init -)"
--- system/_path.zsh
+++ system/_path.zsh
@@ -1,2 +1,2 @@
-export PATH="./bin:/usr/local/bin:/usr/local/sbin:$ZSH/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:$PATH"
+export PATH="~/bin:./bin:/usr/local/bin:/usr/local/sbin:$ZSH/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:$PATH"
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
