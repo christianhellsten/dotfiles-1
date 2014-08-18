d4d903011b0ebc118800c543bfab569d7d7f74f9
christianhellsten <christian@aktagon.com>
2014-08-18 08:18:27 +0300

    Vundle & VIM.

--- system/aliases.zsh
+++ system/aliases.zsh
@@ -1,5 +1,5 @@
-alias ls="ls -lsF --color"
-alias l="ls -lAh --color"
-alias ll="ls -l --color"
-alias la='ls -A --color'
-alias ack=ack-grep
+alias ls="ls -lart"
+#alias l="ls -lAh --color"
+#alias ll="ls -l --color"
+#alias la='ls -A --color'
+#alias ack=ack-grep
---
+++ vim/install.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+#
+# Vundle
+#
+
+# Check for Homebrew
+if [ ! -d "~/.vim/bundle/vundle" ]; then
+  echo "  Installing Vundle for you."
+  git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/vundle
+fi
+
+exit 0
