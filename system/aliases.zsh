af1b7fc5bd98a1ca21b8e16f002224ae091fad7c
christianhellsten <christian@aktagon.com>
2014-09-27 00:21:47 +0300

    ...

--- system/aliases.zsh
+++ system/aliases.zsh
@@ -1,7 +1,8 @@
 alias ls="ls -lart"
 # What's my IP?
 alias wanip='dig +short myip.opendns.com @resolver1.opendns.com'
-
+# Recursive grep
+alias rgrep="grep -r"
 # Fix ack
 alias ack='ACK_PAGER_COLOR="less -x4SRFX" /usr/local/bin/ack'
 
