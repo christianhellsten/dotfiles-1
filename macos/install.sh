9212263a1f1c6bd9f28322046b90d13a98b14195
Christian Hellsten <christian@aktagon.com>
2020-08-22 17:10:12 +0300

    LOL

--- macos/install.sh
+++ macos/install.sh
@@ -1,2 +1,5 @@
 mkdir ~/Library/KeyBindings/
 cp macos/DefaultKeyBinding.dict ~/Library/KeyBindings/DefaultKeyBinding.dict
+
+# LOL, .DS_STORE, LOL
+defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool TRUE
