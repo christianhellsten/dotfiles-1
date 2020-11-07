17c007c0ee9fe51c951f41bc8aac05ee59e13d55
Christian Hellsten <christian@aktagon.com>
2020-11-07 19:11:15 +0200

    Add Talisman
    Talisman is a tool that installs a hook to your repository to ensure that potential secrets or sensitive information do not leave the developer's workstation.

---
+++ security/install.sh
@@ -0,0 +1,2 @@
+# Talisman validates the outgoing changeset for things that look suspicious - such as authorization tokens and private keys
+curl --silent  https://raw.githubusercontent.com/thoughtworks/talisman/master/global_install_scripts/install.bash > /tmp/install_talisman.bash && /bin/bash /tmp/install_talisman.bash
