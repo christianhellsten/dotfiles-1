eedf6a812e959dedf37ddcb80e98c97a456d33b2
Christian Hellsten <christian.hellsten@aktagon.com>
2024-01-19 17:57:55 +0200

    :fire

--- system/aliases.zsh
+++ system/aliases.zsh
@@ -1,8 +1,8 @@
 alias ls="ls -lah"
 #alias docker="podman"
 #alias docker-compose="podman-compose"
-alias vim='/opt/homebrew/bin/nvim'
-alias vi='/opt/homebrew/bin/nvim'
+alias vim='/usr/local/bin/nvim'
+alias vi='/usr/local/bin/nvim'
 # What's my IP?
 alias wanip='dig +short myip.opendns.com @resolver1.opendns.com'
 alias ipaddr='wanip && ipconfig getifaddr en1'
